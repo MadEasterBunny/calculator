9d1f0fcba92ca1a82e300fbc1dd0b0dadf3bc162
MadEasterBunny <40764200+MadEasterBunny@users.noreply.github.com>
2025-11-16 10:23:14 +0900

    Add button attributes and click functionality

--- script.js
+++ script.js
@@ -1,20 +1,66 @@
 const container = document.getElementById("container");
+const display = document.getElementById("display");
 
 const buttons = ["&#x232B;", "C", "7", "8", "9", "/", "4", "5", "6", "*", "1", "2", "3", "-", "0", ".", "=", "+"];
+const numpad = [".", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 const operators = ["+", "-", "*", "/"];
+const equal = ["="];
+const clearDelBtns = ["&#x232B;", "C"];
 
 for(let i = 0; i < buttons.length; i++) {
     const button = document.createElement("button");
     button.innerHTML = buttons[i];
     button.value = buttons[i]
 
-    if(operators.includes(button.value)) {
-        button.classList.add("operator");
+    if(numpad.includes(button.value)) {
+        button.setAttribute("data-type", "number");
     }
 
-    if(["&#x232B;", "C"].includes(button.value)) {
-        button.classList.add("clearDelBtn");
+    if(operators.includes(button.value)) {
+        button.classList.add("operator");
+        button.setAttribute("data-type", "operator");
     }
-    
+
+    if(equal.includes(button.value)) {
+        button.setAttribute("data-type", "utility");
+        button.setAttribute("data-action", "equal");
+    }
+
+    if(clearDelBtns.includes(button.value)) {
+        button.classList.add("clearDelBtn");
+        button.setAttribute("data-type", "utility");
+        if(button.value === "&#x232B;") {
+            button.setAttribute("data-action", "delete");
+        } else {
+            button.setAttribute("data-action", "clear");
+        }
+    }
+
     container.appendChild(button);
 }
+
+container.addEventListener("click", e => {
+    const target = e.target;
+
+    if(target.tagName !== "BUTTON") {
+        return;
+    }
+    
+    const type = target.getAttribute("data-type");
+    const value = target.value;
+    const action = target.getAttribute("data-action");
+
+    switch(type) {
+        case "number":
+            console.log(value);
+            break;
+        case "operator":
+            console.log(value);
+            break;
+        case "utility":
+            console.log(value);
+            break;
+    }
+
+
+})
